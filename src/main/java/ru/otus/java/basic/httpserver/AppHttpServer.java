6b8cebec7fa76a55785fab667b30bf821d9ddf2a
Alex Ardashov <ardashov.alex@ya.ru>
2026-04-05 21:42:59 +0300

    feat: implement multithreaded request processing using ExecutorService

--- src/main/java/ru/otus/java/basic/httpserver/AppHttpServer.java
+++ src/main/java/ru/otus/java/basic/httpserver/AppHttpServer.java
@@ -3,8 +3,8 @@ package ru.otus.java.basic.httpserver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Application {
-    private static final Logger logger = LogManager.getLogger(Application.class.getName());
+public class AppHttpServer {
+    private static final Logger logger = LogManager.getLogger(AppHttpServer.class.getName());
 
     public static void main(String[] args) {
         new HttpServer(8189).start();
--- src/main/java/ru/otus/java/basic/httpserver/HttpServer.java
+++ src/main/java/ru/otus/java/basic/httpserver/HttpServer.java
@@ -2,12 +2,13 @@ package ru.otus.java.basic.httpserver;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import org.apache.logging.log4j.Logger;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class HttpServer {
@@ -15,6 +16,7 @@ public class HttpServer {
     private int port;
     private Dispatcher dispatcher;
 
+    ExecutorService threadPools = Executors.newFixedThreadPool(5);
 
     public HttpServer(int port) {
         this.port = port;
@@ -25,17 +27,30 @@ public class HttpServer {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             logger.log(Level.INFO, "server started");
 
-            try (Socket socket = serverSocket.accept()) {
-                logger.info("new client connected");
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                if (n < 1) {
-                    return;
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    logger.info("new client connected");
+
+                    threadPools.execute(() -> {
+                        try (socket) {
+                            byte[] buffer = new byte[8192];
+                            int n = socket.getInputStream().read(buffer);
+                            if (n < 1) {
+                                return;
+                            }
+                            String rawRequest = new String(buffer, 0, n);
+                            HttpRequest request = new HttpRequest(rawRequest);
+                            request.info(true);
+                            dispatcher.execute(request, socket.getOutputStream());
+                        } catch (IOException e) {
+                            logger.error("IO exception", e);
+                        }
+                        ;
+                    });
+                } catch (IOException e) {
+                    logger.error("client disconnected", e);
                 }
-                String rawRequest = new String(buffer, 0, n);
-                HttpRequest request = new HttpRequest(rawRequest);
-                request.info(true);
-                dispatcher.execute(request, socket.getOutputStream());
             }
         } catch (IOException e) {
             logger.error("failed to start server on port 8189", e);
